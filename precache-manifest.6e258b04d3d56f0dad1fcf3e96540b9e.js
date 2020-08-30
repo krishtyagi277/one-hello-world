809a9425aa804bdf8e6e09b916595015db3f6c55
krishna tyagi <35443292+krishtyagi277@users.noreply.github.com>
2020-08-30 16:41:23 +0530

    Updates

---
+++ precache-manifest.6e258b04d3d56f0dad1fcf3e96540b9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56e3028b0ee368264e26eb91f88dec6",
+    "url": "/one-hello-world/index.html"
+  },
+  {
+    "revision": "6558dc61801940308ee8",
+    "url": "/one-hello-world/static/css/main.8f96f450.chunk.css"
+  },
+  {
+    "revision": "0cbc9dd056c958a1b0e2",
+    "url": "/one-hello-world/static/js/2.0f81383c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/one-hello-world/static/js/2.0f81383c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6558dc61801940308ee8",
+    "url": "/one-hello-world/static/js/main.bf569d4f.chunk.js"
+  },
+  {
+    "revision": "157aec836bb74bb7c72a",
+    "url": "/one-hello-world/static/js/runtime-main.8fc0d677.js"
+  }
+]);
